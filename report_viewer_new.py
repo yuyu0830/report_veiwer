638918c6b7fca46488d5c78806804044c4fd1466
yuyu0830 <woosangwon98@gmail.com>
2021-12-17 13:18:10 +0900

    21-12-17

---
+++ report_viewer_new.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def html_reset():
+    html = driver.page_source
+    return bs(html, 'html.parser')
+
+#ID, 비밀번호 입력
+ID = '20210472'
+PWD = 'bloger1541'
+
+#변수 선언
+count = 0
+all_see = True
+subject = [[], [], []] #대분류, 전체, 과목
+index = []
+status = []
+selector = ['.term_info', 'div.m-box2:nth-child(2) li', 'div.m-box2:nth-child(2) em']
+selector2 = ['span.wb-week', '.wb-on > div > div.wb-status', '.wb-off > div > div.wb-status']
+class_num = [[], [], []]
+temp_list = [[], [], []]
+
+#드라이버
+webdriver_options = webdriver.ChromeOptions()
+webdriver_options .add_argument('headless')
+driver = webdriver.Chrome(executable_path='chromedriver', options=webdriver_options)
+driver.implicitly_wait(time_to_wait=5)
+driver.get('https://lms.kit.ac.kr/ilos/main/member/login_form.acl')
+i = input()
+driver.find_element(By.ID, 'usr_id').send_keys(ID)
+driver.find_element(By.ID, 'usr_pwd').send_keys(PWD)
+driver.find_element(By.XPATH, '//*[@id="login_btn"]').click()
+
+soup = html_reset()
+
+for i in range(3): #과목 개수 입력
+    temp = soup.select(selector[i])
+    for j in temp:
+        subject[i].append(j.text.replace(' ','').replace('\n', ''))
+        
+for i in subject[0]: #대분류 위치
+    index.append(subject[1].index(i))
+index.append(len(subject[1]))
+
+#과목별 강의 수, 들은 여부 구하기
+for i in range(len(subject[1]) - 1):
+    if i not in index:
+        link = '//*[@id="contentsIndex"]/div[2]/div[2]/ol/li[%d]/em' % (i + 1)
+        if driver.find_element(By.XPATH, link).get_attribute('class') == 'sub_open':
+            driver.find_element(By.XPATH, link).click()
+            soup = html_reset()
+            for j in range(3):
+                temp_list[j] = soup.select(selector2[j])
+            if type(temp_list[0]) != type(None):
+                for j in range(3):
+                    for k in range(len(temp_list[j])):
+                        class_num[j].append(temp_list[j][k].get_text())
+            if class_num[0] != []:
+                for j in range(len(class_num[1])):
+                    if class_num[1][j][1] == class_num[1][j][3]:
+                        class_num[1][j] = 1
+                    else:
+                        class_num[1][j] = 0
+                for j in range(len(class_num[2])):
+                    if class_num[2][j][1] == class_num[2][j][3]:
+                        class_num[2][j] = 1
+                    else:
+                        class_num[2][j] = 0
+            driver.find_element(By.XPATH, '//*[@id="submitList_div"]/div[1]').click()
+            html = driver.page_source
+            soup = bs(html, 'html.parser')
+            report = soup.select_one('span#total_nosubmit').get_text()
+            status.append([class_num[0], class_num[1], class_num[2], report])
+            temp_list = [[], [], []]
+            class_num = [[], [], []]
+            report = ''
+            driver.back()
+        else:
+            status.append([[], [], [], '0'])
+
+#출력
+for i in range(len(subject[1]) - 1):
+    if i in index:
+        print("%s" % subject[0][count])
+        count += 1
+    else:
+        print("  %s" % subject[2][i - count])
+        if status[i - count][0] == []:
+            print("    영상 없음")
+        else:
+            for j in range(len(status[i - count][1])):
+                if status[i - count][1][j] == 0:
+                    print("    %4s차 영상 미시청" % status[i - count][0][j])
+                    all_see = False
+            if all_see == True:
+                print("    시청 완료")
+            for j in range(len(status[i - count][2])):
+                print("    %4s차 영상 미개봉" % status[i - count][0][j + len(status[i - count][1])])
+        if status[i - count][3] != '0':
+            print("    미제출 과제 있음")
+        else:
+            print("    과제 제출 완료")
+    all_see = True
---
+++ report_viewer_old.py
@@ -0,0 +1,162 @@
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+
+driver_location = '/Users/yuyu0/Downloads/chromedriver' #드라이버 위치
+#\ 대신 /(슬래시), C:\User\~~~ 일 경우 /User/~~ 부터
+#예시 ) /Users/yuyu0/Downloads/chromedriver
+
+#화면 없이 코드
+webdriver_options = webdriver.ChromeOptions()
+webdriver_options .add_argument('headless')
+
+#변수 설정
+ID = '20210472'
+PWD = 'bloger1541'
+
+#코드 내 사용 변수 설정
+headless = 1  #0 화면 있음, 1 화면 없음
+subject = [] #과목들
+subject_temp = [] #과목 임시
+subject_all = [] #전과목 + 과목묶음
+cal = 0
+cal2 = 0
+status = [] #영상 듣기 현황
+status_temp = [] #현황 임시
+report = [] #과제 제출 현황
+report_temp = [] #과제 임시 
+term = [] #과목 묶음
+term_index = [] #과목 임시
+temp = [] #임시
+class_num1 = ''
+class_num2 = ''
+report_num = ''
+temp_text1 = ''
+temp_text2 = ''
+temp_text3 = 0
+unopen = []
+
+#화면, 드라이버 설정
+if headless == 1:
+    driver = webdriver.Chrome(driver_location, options=webdriver_options)
+else:
+    driver = webdriver.Chrome(driver_location)
+driver.implicitly_wait(time_to_wait=5)
+
+#사이트 목록
+login = 'https://lms.kit.ac.kr/ilos/main/member/login_form.acl'
+
+#로그인 페이지
+driver.get(login)
+driver.find_element_by_name('usr_id').send_keys(ID)
+driver.find_element_by_name('usr_pwd').send_keys(PWD)
+driver.find_element_by_xpath('//*[@id="login_btn"]').click()
+
+#메인 페이지
+html = driver.page_source
+soup = bs(html, 'html.parser')
+
+
+#과목 수 구하기
+temp = soup.select('.term_info') #과목 묶음 개수
+for i in range(len(temp)):
+    term.append(temp[i].text)
+
+temp = soup.select('div.m-box2:nth-child(2) > ol > li')
+for i in range(len(temp)): #임시 리스트 정리
+    temp_text1 = temp[i].text.replace(' ','')
+    if temp_text1 != '':
+        subject_all.append(temp_text1.replace('\n', ''))
+
+temp = soup.select('.sub_open') #수강과목 개수
+for i in range(len(temp)):
+    temp_text1 = temp[i].text.replace(' ','')
+    subject_temp.append(temp_text1.replace('\n', ''))
+
+temp = []
+if len(term) != 1: #과목 묶음별로 과목 나누기
+    for i in term:
+        term_index.append(subject_all.index(i))
+    term_index.append(len(subject_all))
+    for i in range(len(term_index) - 1):
+        for j in range(term_index[i] - i, term_index[i+1] - (1 + i)):
+            temp.append([subject_temp[j], 0])
+        subject.append(temp)
+        temp = []
+else:
+    for i in range(len(subject_temp)):
+        temp.append([subject_temp[i], 0])
+    subject.append(temp)
+
+temp = []
+del term_index
+del subject_temp
+del subject_all
+
+cal2 = 2
+#과목별 강의 수, 들은 여부 구하기
+for i in range(len(term)): #과목묶음으로 묶기
+    for j in range(len(subject[i])): #과목으로 묶기
+        driver.find_element_by_xpath('//*[@id="contentsIndex"]/div[2]/div[2]/ol/li[{}]/em'.format(cal2)).click()
+        html = driver.page_source
+        soup = bs(html, 'html.parser')
+        class_num1 = soup.select('span.wb-week')
+        class_num2 = soup.select('.wb-on > div > div.wb-status')
+        class_num3 = soup.select('.wb-off > div > div.wb-status')
+        report_num = soup.select('div#submitList_div .site-font-color')
+        for l in range(len(class_num1)): #강의 개수만큼 반복
+            if cal < len(class_num2):
+                temp_text1 = class_num1[l].text.replace(' ', '')
+                temp_text2 = class_num2[l].text.replace(' ', '')
+                if temp_text2[0] == temp_text2[2]:
+                    temp_text3 = 0 #시청
+                else:
+                    temp_text3 = 1 #미시청
+                temp.append([temp_text1, temp_text2, temp_text3])
+                cal += 1
+            elif cal >= len(class_num2):
+                temp_text1 = class_num1[l].text.replace(' ', '')
+                temp_text2 = class_num3[l - cal].text.replace(' ', '')
+                temp_text3 = 2 #미공개
+                temp.append([temp_text1, temp_text2, temp_text3])
+                cal += 1
+        if len(report_num) >= 2:
+            report_temp.append([int(report_num[0].text), int(report_num[1].text.replace('%', ''))])
+        else:
+            report_temp.append([0, 0])
+        cal = 0
+        status_temp.append(temp)
+        temp = []
+        cal2 += 1
+        driver.back()
+    report.append(report_temp)
+    status.append(status_temp)
+    report_temp = []
+    status_temp = []
+    cal2 += 1
+
+del status_temp
+cal = 0
+for i in range(len(status)):
+    print("\n{}".format(term[i]))
+    for j in range(len(status[i])):
+        print("\n과목 : {}".format(subject[i][j][0]))
+        print("    시청 여부")
+        for k in range(len(status[i][j])):
+            if status[i][j][k][2] == 1:
+                print("        {}차 시청 미완료".format(status[i][j][k][0]))
+                cal += 1
+            elif status[i][j][k][2] == 2:
+                print("        {}차 미개봉".format(status[i][j][k][0]))
+                cal += 1
+        if cal == 0:
+            print("      과목 시청 완료")
+        else:
+            cal = 0
+        print("    과제 여부")
+        if report[i][j][0] != 0:
+            if report[i][j][1] != 100:
+                print("        과제 미제출")
+            else:
+                print("        과제 제출 완료")
+        else:
+            print("        과제 없음")
